667368be0fca59577e8ebf64d2ed43442e7eb2a8
Nicola Leone Ciardi <nicolaleone.ciardi@gmail.com>
2024-11-10 21:45:42 +0100

    fix[queries]

--- sql/queries/chart_success_prediction.sql
+++ sql/queries/chart_success_prediction.sql
@@ -1,17 +1,26 @@
 -- 5. Predicting Chart Success with Musical Attributes --
 -- Query: Retrieve average values of danceability, energy, valence, and speechiness for songs in different chart ranges (e.g., top 10, 10-50, 50-100). --
 
-SELECT CASE 
-           WHEN RANK() OVER (ORDER BY t.streams DESC) <= 10 THEN 'Top 10'
-           WHEN RANK() OVER (ORDER BY t.streams DESC) <= 50 THEN 'Top 10-50'
-           WHEN RANK() OVER (ORDER BY t.streams DESC) <= 100 THEN 'Top 50-100'
-           ELSE 'Below 100'
-       END AS chart_range,
-       AVG(ms.danceability) AS avg_danceability,
-       AVG(ms.energy) AS avg_energy,
-       AVG(ms.valence) AS avg_valence,
-       AVG(ms.speechiness) AS avg_speechiness
-FROM track t
-JOIN MusicalStats ms ON t.name = ms.track_name
+SELECT chart_range,
+       AVG(d) AS avg_danceability,
+       AVG(e) AS avg_energy,
+       AVG(v) AS avg_valence,
+       AVG(s) AS avg_speechiness
+FROM (
+    SELECT CASE 
+               WHEN tc.count <= 10 THEN 'Top 10'
+               WHEN tc.count <= 50 THEN 'Top 10-50'
+               WHEN tc.count <= 100 THEN 'Top 50-100'
+               ELSE 'Below 100'
+           END AS chart_range,
+           ms.danceability AS d,
+           ms.energy AS e,
+           ms.valence AS v,
+           ms.speechiness AS s,
+           tc.count AS rank
+    FROM track t
+    JOIN MusicalStats ms ON t.id = ms.track_id
+    JOIN Track_Chart tc ON t.id = tc.track_id
+) AS subquery
 GROUP BY chart_range
-ORDER BY chart_range;
+ORDER BY rank;
